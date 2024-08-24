a50d574a99f8776dff1bb13270330552a9c85ddc
Yola Rivers <yolarivers@gmail.com>
2024-08-24 13:24:35 -0500

    added getters and setters to POIs

--- JPAParkItLikeItsHot/src/main/java/com/skilldistillery/nationalparks/entities/PointOfInterestComment.java
+++ JPAParkItLikeItsHot/src/main/java/com/skilldistillery/nationalparks/entities/PointOfInterestComment.java
@@ -31,17 +31,75 @@ public class PointOfInterestComment {
     @Column(name="image_url")
     private String imageUrl;
 
-    @Column(name="user_id")
+    @Column(name="user_id", insertable = false, updatable = false)
     private int userId;
 
     @ManyToOne
     @JoinColumn(name="point_of_interest_id")
    	private PointOfInterest interests;
 
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public PointOfInterest getInterests() {
+        return interests;
+    }
+
+    public void setInterests(PointOfInterest interests) {
+        this.interests = interests;
+    }
+
     @Override
-	public String toString() {
-		return "PointOfInterestComment [id=" + id + ", createDate=" + createDate + ", lastUpdate=" + lastUpdate
-				+ ", content=" + content + ", imageUrl=" + imageUrl + ", userId=" + userId + ", interests=" + interests
-				+ "]";
-	}
+    public String toString() {
+        return "PointOfInterestComment [id=" + id + ", createDate=" + createDate + ", lastUpdate=" + lastUpdate
+                + ", content=" + content + ", imageUrl=" + imageUrl + ", userId=" + userId + ", interests=" + interests
+                + "]";
+    }
 }
--- JPAParkItLikeItsHot/src/main/java/com/skilldistillery/nationalparks/entities/PointOfInterestType.java
+++ JPAParkItLikeItsHot/src/main/java/com/skilldistillery/nationalparks/entities/PointOfInterestType.java
@@ -25,8 +25,50 @@ public class PointOfInterestType {
     @Column(name="image_url")
     private String imageUrl;
     
-    @ManyToMany(mappedBy="pointOfInterest")
+    @ManyToMany(mappedBy="interestTypes")
 	private List<PointOfInterest> interests;
+
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public List<PointOfInterest> getInterests() {
+        return interests;
+    }
+
+    public void setInterests(List<PointOfInterest> interests) {
+        this.interests = interests;
+    }
 
     @Override
     public String toString() {
