5a16aa45526775d5c14dd78b3fae3f684dc40116
DesKenn <destineek423@gmail.com>
2024-08-26 11:45:58 -0600

    junit tests bugs fixed

---
+++ JPAParkItLikeItsHot/src/test/java/com/skilldistillery/nationalparks/entities/NationalParkTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.nationalparks.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class NationalParkTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private NationalPark park;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAParkItLikeItsHot");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		park = em.find(NationalPark.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+
+	@Test
+	void test_User_entity_mapping() {
+		assertNotNull(park);
+		assertEquals("Rocky Mountain", park.getName());
+	}
+
+}
