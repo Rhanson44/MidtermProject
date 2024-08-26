e554cf359035a4db12815f376e355b80baced359
Ryan Hanson <hansonrj8322@gmail.com>
2024-08-26 12:57:58 -0600

    test commit

--- JPAParkItLikeItsHot/src/test/java/com/skilldistillery/nationalparks/entities/PointOfInterestTypeTest.java
+++ JPAParkItLikeItsHot/src/test/java/com/skilldistillery/nationalparks/entities/PointOfInterestTypeTest.java
@@ -1,11 +1,7 @@
 package com.skilldistillery.nationalparks.entities;
 
-<<<<<<< HEAD
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-=======
-import static org.junit.jupiter.api.Assertions.*;
->>>>>>> 63088f072074edc96b192fa257b0c75fbdcfd624
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -18,18 +14,11 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 class PointOfInterestTypeTest {
-<<<<<<< HEAD
 	
 	private static EntityManagerFactory emf;
 	private EntityManager em;
 	private PointOfInterestType poiType;
-=======
-
-	private static EntityManagerFactory emf;
-	private EntityManager em;
-	private PointOfInterestType poi;
->>>>>>> 63088f072074edc96b192fa257b0c75fbdcfd624
-
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		emf = Persistence.createEntityManagerFactory("JPAParkItLikeItsHot");
@@ -43,31 +32,19 @@ class PointOfInterestTypeTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		em = emf.createEntityManager();
-<<<<<<< HEAD
 		poiType = em.find(PointOfInterestType.class, 1);
-=======
-		poi = em.find(PointOfInterestType.class, 1);
->>>>>>> 63088f072074edc96b192fa257b0c75fbdcfd624
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		em.close();
-<<<<<<< HEAD
-=======
-		poi = null;
->>>>>>> 63088f072074edc96b192fa257b0c75fbdcfd624
+		poiType = null;
 	}
 
 	@Test
 	void test_User_entity_mapping() {
-<<<<<<< HEAD
 		assertNotNull(poiType);
 		assertEquals( "Visitor Center", poiType.getName());
-=======
-		assertNotNull(poi);
-		assertEquals("Visitor Center", poi.getName());
->>>>>>> 63088f072074edc96b192fa257b0c75fbdcfd624
 	}
 
 }
